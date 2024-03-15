f713b9b72ca7ea510cdbb242715c80cebc8ed3b4
Vincent <vincent.wijaya@mekari.com>
2024-03-15 21:51:29 +0700

    finished logic for FindOneProduct in service

--- service/product.go
+++ service/product.go
@@ -1,6 +1,9 @@
 package service
 
-import "gin-railway/repository"
+import (
+	"gin-railway/core"
+	"gin-railway/repository"
+)
 
 type ProductService struct {
 	productRepo repository.ProductRepo
@@ -10,4 +13,18 @@ func NewService(productRepo repository.ProductRepo) *ProductService {
 	return &ProductService{
 		productRepo: productRepo,
 	}
+}
+
+func (s *ProductService) FindOneProduct(id int) (*core.FindOneProductResponse, error) {
+	product, err := s.productRepo.FindProductByID(uint(id))
+	if err != nil {
+		return nil, err
+	}
+
+	resp := &core.FindOneProductResponse{
+		ProductName: product.Name,
+		Quantity:    product.Quantity,
+	}
+
+	return resp, nil
 }
