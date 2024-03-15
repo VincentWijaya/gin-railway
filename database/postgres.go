44448d6a773898cb3b11b1239a6712f8f88ea24a
Vincent <vincent.wijaya@mekari.com>
2024-03-15 21:48:03 +0700

    create function to find product by id in product repo

--- database/postgres.go
+++ database/postgres.go
@@ -9,8 +9,8 @@ import (
 )
 
 type Postgres struct {
-	db  *gorm.DB
-	err error
+	DB  *gorm.DB
+	Err error
 }
 
 func NewPostgres() *Postgres {
@@ -21,7 +21,7 @@ func NewPostgres() *Postgres {
 	db, err := gorm.Open(postgres.Open(dsn), nil)
 
 	return &Postgres{
-		db:  db,
-		err: err,
+		DB:  db,
+		Err: err,
 	}
 }
--- repository/product.go
+++ repository/product.go
@@ -6,15 +6,18 @@ import (
 )
 
 type ProductRepo struct {
-	db *database.Postgres
-}
-
-type ProductImpl interface {
-	FindProductByID(ID int64) (*core.Product, error)
+	postgres *database.Postgres
 }
 
 func NewProductRepo(db *database.Postgres) *ProductRepo {
 	return &ProductRepo{
-		db: db,
+		postgres: db,
 	}
+}
+
+func (r *ProductRepo) FindProductByID(id uint) (*core.Product, error) {
+	product := core.Product{}
+	err := r.postgres.DB.First(&product, "id=?", id).Error
+
+	return &product, err
 }
